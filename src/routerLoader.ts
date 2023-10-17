a4b6cbd0666dc9fd55f1a40bbae64a575300cfd8
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-10-17 14:38:19 -0300

    feat: insert types in routerLoader

--- src/routerLoader.ts
+++ src/routerLoader.ts
@@ -1,7 +1,8 @@
+import { Express } from 'express-serve-static-core'
 import path from 'path'
 import fs from 'fs'
 
-export const routerLoader = (app) => {
+export const routerLoader = (app: Express): void => {
   const modulesPath = path.join(__dirname, 'modules')
 
   fs.readdirSync(modulesPath).forEach(async (dir) => {
@@ -9,8 +10,10 @@ export const routerLoader = (app) => {
 
     if (fs.statSync(modulesPath).isDirectory()) {
       const controllerPath = path.join(modulePath, `${dir}.controller.ts`)
+
       if (fs.existsSync(controllerPath)) {
         const controller = await import(controllerPath)
+
         if (controller.default && typeof controller.default === 'function') {
           app.use(controller.default)
         }

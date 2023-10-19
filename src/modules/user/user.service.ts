311af7bdc077d0c9292c1b2fc6dd9dad642c6a34
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-10-19 10:34:46 -0300

    feat: insert validation in createUser

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { UserModel } from './user.model'
 import { userInsertDTO } from './dto/user-insert.dto'
 import { NotFoundException } from '@exceptions/not-found-exception'
+import { BadRequestException } from '@exceptions/bad-request-exception'
 
 const prisma = new PrismaClient()
 
@@ -14,7 +15,24 @@ export const getUser = async (): Promise<UserModel[]> => {
   return users
 }
 
+const getUserByCpfOurEmail = async (cpf: string, email: string): Promise<UserModel> => {
+  const user = prisma.user.findFirst({
+    where: {
+      OR: [{ cpf }, { email }],
+    },
+  })
+
+  if (!user) {
+    throw new NotFoundException('User')
+  }
+  return user
+}
+
 export const createUser = async (body: userInsertDTO): Promise<UserModel> => {
+  const user = await getUserByCpfOurEmail(body.cpf, body.email)
+  if (user) {
+    throw new BadRequestException('email or cpj already exists in the DB')
+  }
   return prisma.user.create({
     data: body,
   })
